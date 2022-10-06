6617388a0889ec8c8e2edbe0631cfdbffceb94ba
Vilela <47867188+gcvilela@users.noreply.github.com>
2022-10-06 19:57:38 -0300

    Add files via upload

---
+++ Apache-tomcat/stopTomcat.bat
@@ -0,0 +1,14 @@
+% Execução nos laboratórios do IFMG
+%set JAVA_HOME=C:\Program Files (x86)\Java\jre1.8.0_321
+%set CATALINA_HOME=C:\Users\geraldo.souza\java\apache-tomcat-8.5.82
+
+%cd \
+%cd %CATALINA_HOME%\bin
+
+% Execução na máquina local
+
+% Variáveis JAVA_HOME E CATALINA_HOME já configuradas no windows e ou linux
+
+cd \
+cd %CATALINA_HOME%\bin
+catalina.bat stop
